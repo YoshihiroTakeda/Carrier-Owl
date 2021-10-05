49fa460ca7ae9350a3382cdd4767b41355ae88f7
YoshiInTokyo <49469846+YoshiInTokyo@users.noreply.github.com>
2021-10-05 14:22:27 +0900

    [debug]

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -139,7 +139,8 @@ def send2app(text: str, slack_channel: str, line_token: str) -> None:
             response = client.chat_postMessage(
                 channel=slack_channel,
                 text=text,
-                unfurl_links=False
+                unfurl_links=False,
+                mrkdwn=False
             ) 
         except SlackApiError as e:
             # You will get a SlackApiError if "ok" is False
