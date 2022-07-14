bcbc6d543d5cd3dc0034f5c0e4fcb881fef3ab73
YoshiInTokyo <49469846+YoshiInTokyo@users.noreply.github.com>
2022-07-14 17:00:08 +0900

    [debug]print soup

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -290,6 +290,7 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
 
 def get_text_from_page_source(html: str) -> str:
     soup = BeautifulSoup(html, features='lxml')
+    print(soup)
     target_elem = soup.find(class_="lmt__translations_as_text__text_btn")
     text = target_elem.text
     text = ' '.join(text.split())
