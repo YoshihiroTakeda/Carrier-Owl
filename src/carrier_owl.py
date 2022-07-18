28a2a031e89442f79b0474110fbdf7a4b2f9b04b
YoshiInTokyo <49469846+YoshiInTokyo@users.noreply.github.com>
2022-07-18 17:07:37 +0900

    [revert]

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -268,16 +268,13 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
 
     driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
     driver.get(url)
-    if driver.find_element_by_class_name("lmt__translations_as_text__text_btn") is not None:
+    
+    for i in range(50):
+        # 指定時間待つ
+        time.sleep(sleep_time)
+        print(driver.find_element_by_class_name("lmt__translations_as_text__text_btn"))
         html = driver.page_source
-        to_text = get_text_from_page_source(html)        
-
-#     for i in range(50):
-#         # 指定時間待つ
-#         time.sleep(sleep_time)
-#         # 翻訳要素が存在していることを確認
-        
-
+        to_text = get_text_from_page_source(html)
 
         if to_text:
             break
